841550ca56c313abede81467462a155576a04d98
DekusDenial <dekusdenial@hotmail.com>
2012-04-07 22:18:25 -0400

    added attributes support functions

--- utils.js
+++ utils.js
@@ -7,3 +7,30 @@ function anotherTrim(raw) {
     return false;
   }
 }
+
+function elementSupportsAttribute(element, attribute) {
+  return (attribute in document.createElement(element));
+};
+
+if (!elementSupportsAttribute('textarea', 'placeholder')) {
+  // Fallback for browsers that don't support HTML5 placeholder attribute
+  // assume $titles = <legend> elements, $txt = all elements should havte placeholder attribute
+  var $titles = $('legend'), $txt = $titles.siblings();
+  $.each($titles, function(_, title) {
+    $txt.filter('#' + $(title).attr('title'))
+        .data('text', 'Enter your ' + title.innerText + ' here...')
+        .css('color', '#999')
+
+        //key parts follow, using data-text custom attributes
+        .on('focus', function() {
+          if (this.value == $(this).data('text')) {
+            this.value = '';
+          }
+        })
+        .on('blur', function() {
+          if (this.value == '') {
+            this.value = $(this).data('text');
+          }
+        });
+  });
+}
